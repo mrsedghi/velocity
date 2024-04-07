15cfbccb76ecca366ee12ae3ad6c70bb9761596a
Mohammad Reza Sedgi <m.r.sedghii@gmail.com>
2024-04-07 14:13:22 +0330

    fix bugs and add color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import colorToTailwindClass from "./toTailwind";
+import react, { useEffect, useState } from "react";
 import ColorPicker from "@radial-color-picker/react-color-picker";
 import "@radial-color-picker/react-color-picker/dist/style.css";
+import axios from "axios";
 function App() {
-  const [mod, setMod] = useState("");
-  const [linemod, setLinemod] = useState("bg-black");
-  const [light, setLight] = useState(1);
+  const [mod, setMod] = useState(["#882288", "#228888", "#882222", "#828282"]);
+  const [index, setIndex] = useState(3);
   const [color, setColor] = useState({
     hue: 90,
     saturation: 100,
@@ -25,48 +26,97 @@ function App() {
     return `#${f(0)}${f(8)}${f(4)}`;
   }
 
+  const sendToEsp = async () => {
+    try {
+      const mainUrl = "192.168.4.1";
+      const response = await axios.get(
+        mainUrl + "/loop=" + index,
+        "&effect=1&color1=",
+        color.hue,
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error reading customer :", error);
+    }
+  };
+  useEffect(() => {
+    console.log(color);
+    if (!color) return;
+    const tl = hslToHex(color.hue, 100, 50); // colorToTailwindClass(hslToHex(hue, 100, 50));
+
+    var thisMode = mod;
+    console.log("index:", index, "mode:", thisMode, "hue:", color.hue);
+    // Code to execute after index state update is complete
+    const newMod = [...mod];
+    newMod[index] = tl;
+    setMod(newMod);
+
+    console.log("index:", index, "mode:", thisMode);
+    sendToEsp();
+  }, [color.hue]);
   const onInput = (hue) => {
     setColor((prev) => ({ ...prev, hue }));
-
-    setMod("border-[" + hslToHex(hue, 50, 50) + "]");
-
-    console.log("border-[" + hslToHex(hue, 50, 50) + "]");
   };
 
   return (
     <>
-      <div className=" flex flex-col justify-center items-center m-10">
-        <div className="flex flec-row m-10 gap-5 items-center ">
-          <button
-            className={`w-32 h-32 rounded-full border-8  ${light == 1 && mod}`}
-            onClick={() => {
-              setLight(1);
-              console.log(mod);
-            }}
-          ></button>
+      <div className="m-0 p-10 bg-black w-full h-full">
+        <div className=" flex flex-col justify-center items-center m-10">
+          <div className="flex flec-row m-10 gap-5 items-center ">
+            <button
+              className={`w-32 h-32 rounded-full border-[12px]  ${
+                index == 0 && "shadow-lg shadow-blue-500 bg-blue-700/30"
+              }`}
+              style={{ borderColor: mod[0] }}
+              onClick={() => {
+                setIndex(0);
+                console.log("index:", index, mod);
+              }}
+            ></button>
+
+            <button
+              className={`w-32 h-32 rounded-full border-[12px] ${
+                index == 1 && "shadow-lg shadow-blue-500 bg-blue-700/30"
+              }`}
+              style={{ borderColor: mod[1] }}
+              onClick={() => {
+                setIndex(1);
+                console.log("index:", index, mod);
+              }}
+            ></button>
+            <button
+              className={`w-28 h-28 rounded-full border-[10px] ${
+                index == 2 && " shadow-lg shadow-blue-500 bg-blue-700/30"
+              }`}
+              style={{ borderColor: mod[2] }}
+              onClick={() => {
+                setIndex(2);
+                console.log("index:", index, mod);
+              }}
+            ></button>
+          </div>
 
           <button
-            className={`w-32 h-32 rounded-full border-8 ${light == 2 && mod}`}
+            className={`w-96 h-3  rounded ${
+              index == 3 &&
+              " shadow-lg shadow-blue-500 border-[1px] border-blue-600"
+            }`}
+            style={{ backgroundColor: mod[3] }}
             onClick={() => {
-              setLight(2);
-              console.log(mod);
-            }}
-          ></button>
-          <button
-            className={`w-28 h-28 rounded-full border-8  ${light == 3 && mod}`}
-            onClick={() => {
-              setLight(3);
-              console.log(
-                "w-28 h-28 rounded-full border-8" + light == 3 && mod
-              );
+              setIndex(3);
+              console.log("index:", index, mod);
             }}
           ></button>
         </div>
-
-        <div className={`w-96 h-3 ${linemod} rounded`}></div>
-      </div>
-      <div className=" flex flex-row justify-center gap-5">
-        <ColorPicker {...color} onChange={onInput} />
+        <div className=" flex flex-row justify-center gap-5">
+          <ColorPicker {...color} onChange={onInput} />
+        </div>
       </div>
       {/* <div className=" flex flex-row justify-center gap-5">
         <button
---
+++ src/toTailwind.ts
@@ -0,0 +1,129 @@
+import { type TailwindColors, twColors, type Color } from "./colors";
+import chroma from "chroma-js";
+
+function isColorHexValue(color: string): boolean {
+	return /^#([0-9a-f]{3,8})$/i.test(color);
+}
+
+function hexToRgb(hex: string): Color {
+	if (!chroma.valid(hex)) {
+		throw new Error(`Invalid hex color: ${hex}`);
+	}
+
+	const chromaColor = chroma(hex);
+	const [r, g, b] = chromaColor.rgb();
+	const a = chromaColor.alpha();
+	return { r, g, b, a };
+}
+
+function findEquivalentTailwindClass(
+	hexColor: string,
+	tailwindColors: TailwindColors
+): [string, string] | null {
+	for (const [colorName, color] of Object.entries(tailwindColors)) {
+		if (typeof color === "string") {
+			if (color.toLowerCase() === hexColor.toLowerCase()) {
+				return [colorName, "DEFAULT"];
+			}
+		} else if (typeof color === "object") {
+			for (const [shade, shadeColor] of Object.entries(color)) {
+				if (
+					typeof shadeColor === "string" &&
+					shadeColor.toLowerCase() === hexColor.toLowerCase()
+				) {
+					return [colorName, shade];
+				}
+			}
+		}
+	}
+
+	return null;
+}
+
+function closestColor(
+	inputColor: Color,
+	colors: TailwindColors
+): [string, string] {
+	let closestColorName = "";
+	let closestShade = "";
+	let smallestDistance = Infinity;
+
+	for (const [colorName, color] of Object.entries(colors)) {
+		if (typeof color === "string") {
+			if (!chroma.valid(color)) continue; // skip invalid colors
+
+			const distance = colorDistance(inputColor, hexToRgb(color));
+			if (distance < smallestDistance) {
+				smallestDistance = distance;
+				closestColorName = colorName;
+				closestShade = "DEFAULT";
+			}
+		} else if (typeof color === "object") {
+			for (const [shade, shadeColor] of Object.entries(color)) {
+				if (typeof shadeColor === "string" && !chroma.valid(shadeColor))
+					continue; // skip invalid colors
+				const distance = colorDistance(
+					inputColor,
+					typeof shadeColor === "string" ? hexToRgb(shadeColor) : shadeColor
+				);
+				if (distance < smallestDistance) {
+					smallestDistance = distance;
+					closestColorName = colorName;
+					closestShade = shade;
+				}
+			}
+		}
+	}
+
+	return [closestColorName, closestShade];
+}
+
+function colorDistance(color1: Color, color2: Color): number {
+	//Euclidean distance
+	const dr = color1.r - color2.r;
+	const dg = color1.g - color2.g;
+	const db = color1.b - color2.b;
+	const da = (color1.a || 0) - (color2.a || 0);
+
+	return Math.sqrt(dr * dr + dg * dg + db * db + da * da);
+}
+
+export default function colorToTailwindClass(colorInput: string): string {
+	const isTailwindColorName = Object.keys(twColors).includes(
+		colorInput.toLowerCase()
+	);
+
+	if (isTailwindColorName) {
+		if (
+			colorInput.toLowerCase() === "white" ||
+			colorInput.toLowerCase() === "black"
+		) {
+			return colorInput.toLowerCase();
+		}
+	}
+
+	const isValidColor = chroma.valid(colorInput);
+	if (!isValidColor) throw new Error(`Invalid color input: "${colorInput}"`);
+
+	const hexColor = !isColorHexValue(colorInput)
+		? chroma(colorInput).hex()
+		: colorInput;
+	const rgbColor = hexToRgb(hexColor);
+
+	const result = findEquivalentTailwindClass(hexColor, twColors);
+	if (result !== null) {
+		const [colorName, shade] = result;
+		if (shade === "DEFAULT") {
+			return colorName;
+		}
+		return `${colorName}-${shade}`;
+	} else {
+		const [colorName, shade] = closestColor(rgbColor, twColors);
+
+		if (shade === "DEFAULT") {
+			return colorName;
+		}
+
+		return `${colorName}-${shade}`;
+	}
+}
